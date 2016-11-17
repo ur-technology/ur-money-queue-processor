218012c8e1b6a64ebafa00ea061aebc08266e25c
John Reitano <jreitano@gmail.com>
2016-11-17 10:51:55 -0800

    try splitting into 4 queues

--- lib/authentication.ts
+++ lib/authentication.ts
@@ -5,163 +5,141 @@ import {QueueProcessor} from './queue_processor';
 
 export class AuthenticationQueueProcessor extends QueueProcessor {
   private twilioClient: any; // used to send messages via twilio
+  smsCodeMatchingQueue: any;
+  emailCodeMatchingQueue: any;
 
   init(): Promise<any>[] {
     return [
-      this.ensureQueueSpecLoaded("/authenticationQueue/specs/sms_code_matching", {
-        "start_state": "sms_code_matching_requested",
-        "in_progress_state": "sms_code_matching_in_progress",
-        "finished_state": "sms_code_matching_completed",
-        "error_state": "sms_code_matching_error",
-        "timeout": 24*60*60*1000
-      }),
-      this.ensureQueueSpecLoaded("/authenticationQueue/specs/sms_code_generation", {
-        "start_state": "sms_code_generation_requested",
-        "in_progress_state": "sms_code_generation_in_progress",
-        "finished_state": "sms_code_generation_completed_and_code_sent",
-        "error_state": "sms_code_generation_error",
+      this.ensureQueueSpecLoaded("/smsAuthCodeGenerationQueue/specs/code_generation", {
+        "in_progress_state": "in_progress",
+        "finished_state": "completed",
+        "error_state": "error",
         "timeout": 5*60*1000
       }),
-      this.ensureQueueSpecLoaded("/authenticationQueue/specs/email_code_generation", {
-        "start_state": "email_code_generation_requested",
-        "in_progress_state": "email_code_generation_in_progress",
-        "finished_state": "email_code_generation_completed_and_code_sent",
-        "error_state": "email_code_generation_error",
+      this.ensureQueueSpecLoaded("/smsAuthCodeMatchingQueue/specs/code_matching", {
+        "in_progress_state": "in_progress",
+        "finished_state": "completed",
+        "error_state": "error",
         "timeout": 5*60*1000
       }),
-      this.ensureQueueSpecLoaded("/authenticationQueue/specs/email_code_matching", {
-        "start_state": "email_code_matching_requested",
-        "in_progress_state": "email_code_matching_in_progress",
-        "finished_state": "email_code_matching_completed",
-        "error_state": "email_code_matching_error",
-        "timeout": 24*60*60*1000
+      this.ensureQueueSpecLoaded("/emailAuthCodeGenerationQueue/specs/code_generation", {
+        "in_progress_state": "in_progress",
+        "finished_state": "completed",
+        "error_state": "error",
+        "timeout": 5*60*1000
+      }),
+      this.ensureQueueSpecLoaded("/emailAuthCodeMatchingQueue/specs/code_matching", {
+        "in_progress_state": "in_progress",
+        "finished_state": "completed",
+        "error_state": "error",
+        "timeout": 5*60*1000
       })
     ];
   }
 
   process(): any[] {
-    let queueRef = this.db.ref("/authenticationQueue");
     return [
-      this.processSmsCodeGenerationQueue(queueRef),
-      this.processSmsCodeMatchingQueue(queueRef),
-      this.processEmailCodeGenerationQueue(queueRef),
-      this.processEmailCodeMatchingQueue(queueRef)
+      this.processSmsAuthCodeGenerationQueue(),
+      this.processSmsAuthCodeMatchingQueue(),
+      this.processEmailAuthCodeGenerationQueue(),
+      this.processEmailAuthCodeMatchingQueue()
     ]
   }
 
-  private processSmsCodeGenerationQueue(queueRef: any) {
+  private processSmsAuthCodeGenerationQueue() {
     let self = this;
-    let options = { 'specId': 'sms_code_generation', 'numWorkers': 1, sanitize: false };
+    let options = { 'specId': 'code_generation', 'numWorkers': 1, sanitize: false };
+    let queueRef = self.db.ref('/smsAuthCodeGenerationQueue');
     let queue = new self.Queue(queueRef, options, (task: any, progress: any, resolve: any, reject: any) => {
       self.startTask(queue, task);
 
-      if (task.emailAuthenticationResult && task.emailAuthenticationResult.codeMatch) {
-        self.sendSmsAuthenticationCode(task.phone).then((smsAuthenticationCode: string) => {
-          task.smsAuthenticationCode = smsAuthenticationCode;
-          task._new_state = "sms_code_generation_completed_and_code_sent";
-          self.resolveTask(queue, task, resolve, reject);
-        }, (error) => {
-          self.rejectTask(queue, task, error, reject);
-        });
-        return;
-      }
+      let parentTaskRef = self.db.ref(`/emailAuthCodeMatchingQueue/tasks/${task._id}`);
+      parentTaskRef.once('value').then((snapshot: firebase.database.DataSnapshot) => {
 
-
-      // look up userId of invited user
-      delete task.userId;
-      self.lookupUsersByPhone(task.phone).then((matchingUsers) => {
-        if (_.isEmpty(matchingUsers)) {
-          log.info(`no matching user found for ${task.phone}`);
-          task._new_state = "sms_code_generation_canceled_because_user_not_invited";
-          self.resolveTask(queue, task, resolve, reject);
+        // if the user just authenticated his email, we already have
+        // the userId of an user who was inivited via prefinery
+        let parentTask = snapshot.val();
+        if (parentTask && parentTask.userId) {
+          task.userId = parentTask.userId;
+          self.sendAuthenticationCodeViaSms(task.phone).then((authenticationCode: string) => {
+            task.authenticationCode = authenticationCode;
+            task._new_state = "completed";
+            self.resolveTask(queue, task, resolve, reject);
+          }, (error) => {
+            self.rejectTask(queue, task, error, reject);
+          });
           return;
         }
 
-        let activeUsers = _.reject(matchingUsers, 'disabled');
-        if (_.isEmpty(activeUsers)) {
-          let disabledUser: any = _.first(matchingUsers);
-          log.info(`found matching user ${disabledUser.userId} for ${task.phone} but user was disabled`);
-          task._new_state = "sms_code_generation_canceled_because_user_disabled";
-          self.resolveTask(queue, task, resolve, reject);
-          return;
-        }
+        // look up userId of user via app
+        // delete task.userId;
+        self.lookupUsersByPhone(task.phone).then((matchingUsers) => {
+          if (_.isEmpty(matchingUsers)) {
+            log.info(`no matching user found for ${task.phone}`);
+            task._new_state = "canceled_because_user_not_invited";
+            self.resolveTask(queue, task, resolve, reject);
+            return;
+          }
 
-        // TODO: handle case where there are multiple invitations; for now, choose first user
-        let matchingUser: any = _.first(activeUsers);
-        log.debug(`matching user with userId ${matchingUser.userId} found for ${task.phone}`);
-        task.userId = matchingUser.userId;
-        self.sendSmsAuthenticationCode(task.phone).then((smsAuthenticationCode: string) => {
-          task.smsAuthenticationCode = smsAuthenticationCode;
-          task._new_state = "sms_code_generation_completed_and_code_sent";
-          self.resolveTask(queue, task, resolve, reject);
+          let activeUsers = _.reject(matchingUsers, 'disabled');
+          if (_.isEmpty(activeUsers)) {
+            let disabledUser: any = _.first(matchingUsers);
+            log.info(`found matching user ${disabledUser.userId} for ${task.phone} but user was disabled`);
+            task._new_state = "canceled_because_user_disabled";
+            self.resolveTask(queue, task, resolve, reject);
+            return;
+          }
+
+          // TODO: handle case where there are multiple invitations; for now, choose first user
+          let matchingUser: any = _.first(activeUsers);
+          log.debug(`matching user with userId ${matchingUser.userId} found for ${task.phone}`);
+          task.userId = matchingUser.userId;
+          self.sendAuthenticationCodeViaSms(task.phone).then((authenticationCode: string) => {
+            task.authenticationCode = authenticationCode;
+            task._new_state = "completed";
+            self.resolveTask(queue, task, resolve, reject);
+          }, (error) => {
+            self.rejectTask(queue, task, error, reject);
+          });
         }, (error) => {
           self.rejectTask(queue, task, error, reject);
         });
-      }, (error) => {
-        self.rejectTask(queue, task, error, reject);
       });
     });
     return queue;
   }
 
-  private processEmailCodeGenerationQueue(queueRef: any) {
+  private processSmsAuthCodeMatchingQueue() {
     let self = this;
-    let options = { 'specId': 'email_code_generation', 'numWorkers': 1, sanitize: false };
+    let options = { 'specId': 'code_matching', 'numWorkers': 1, 'sanitize': false };
+    let queueRef = self.db.ref('/smsAuthCodeMatchingQueue');
     let queue = new self.Queue(queueRef, options, (task: any, progress: any, resolve: any, reject: any) => {
       self.startTask(queue, task);
-      delete task.userId;
 
-      self.lookupUsersByEmail(task.email).then((matchingUsers) => {
-        if (_.isEmpty(matchingUsers)) {
-          log.info(`no matching user found for ${task.email}`);
-          task._new_state = "email_code_generation_canceled_because_user_not_invited";
-          self.resolveTask(queue, task, resolve, reject);
+      let parentTaskRef = self.db.ref(`/smsAuthCodeGenerationQueue/tasks/${task._id}`);
+      parentTaskRef.once('value').then((snapshot: firebase.database.DataSnapshot) => {
+        let parentTask = snapshot.val();
+        if (!parentTask) {
+          self.rejectTask(queue, task, `could not find parent task ${parentTaskRef.toString()}`, reject);
           return;
         }
 
-        let activeUsers = _.reject(matchingUsers, 'disabled');
-        if (_.isEmpty(activeUsers)) {
-          let disabledUser: any = _.first(matchingUsers);
-          log.info(`found matching user ${disabledUser.userId} for ${task.email} but user was disabled`);
-          task._new_state = "email_code_generation_canceled_because_user_disabled";
-          self.resolveTask(queue, task, resolve, reject);
-          return;
+        task.authenticationCodeSubmittedByUser = parentTask.authenticationCode;
+        task.phone = parentTask.phone;
+        task.userId = parentTask.userId;
+
+        let codeMatch = task.authenticationCode == task.authenticationCodeSubmittedByUser || (task.phone == '+16199344518' && task.authenticationCodeSubmittedByUser == '923239');
+        task.result = { codeMatch: codeMatch };
+        if (codeMatch) {
+          log.debug(`authenticationCodeSubmittedByUser ${task.authenticationCodeSubmittedByUser} matches actual authenticationCode; sending authToken to user`);
+          task.result.authToken = firebase.auth().createCustomToken(task.userId, { some: "arbitrary", task: "here" });
+        } else {
+          log.debug(`authenticationCodeSubmittedByUser ${task.authenticationCodeSubmittedByUser} does not match actual authenticationCode ${task.authenticationCode}`);
         }
-
-        // TODO: handle case where there are multiple invitations; for now, choose first user
-        let matchingUser: any = _.first(activeUsers);
-        log.debug(`matching user with userId ${matchingUser.userId} found for ${task.email}`);
-        task.userId = matchingUser.userId;
-        self.sendEmailAuthenticationCode(task.email).then((emailAuthenticationCode: string) => {
-          task.emailAuthenticationCode = emailAuthenticationCode;
-          task._new_state = "email_code_generation_completed_and_code_sent";
-          self.resolveTask(queue, task, resolve, reject);
-        }, (error) => {
-          self.rejectTask(queue, task, error, reject);
-        });
-      }, (error) => {
-        self.rejectTask(queue, task, error, reject);
-      });
-    });
-    return queue;
-  }
-
-  private processSmsCodeMatchingQueue(queueRef: any) {
-    let self = this;
-    let options = { 'specId': 'sms_code_matching', 'numWorkers': 1, 'sanitize': false };
-    let queue = new self.Queue(queueRef, options, (task: any, progress: any, resolve: any, reject: any) => {
-      self.startTask(queue, task);
-      let codeMatch = task.submittedSmsAuthenticationCode == task.smsAuthenticationCode || (task.phone == '+16199344518' && task.submittedSmsAuthenticationCode == '923239');
-      task.smsAuthenticationResult = { codeMatch: codeMatch };
-      if (codeMatch) {
-        log.debug(`submittedSmsAuthenticationCode ${task.submittedSmsAuthenticationCode} matches actual smsAuthenticationCode; sending authToken to user`);
-        task.smsAuthenticationResult.authToken = firebase.auth().createCustomToken(task.userId, { some: "arbitrary", task: "here" });
-      } else {
-        log.debug(`submittedSmsAuthenticationCode ${task.submittedSmsAuthenticationCode} does not match actual smsAuthenticationCode ${task.smsAuthenticationCode}`);
-      }
-      let newPhone: string = codeMatch && task.emailAuthenticationResult && task.emailAuthenticationResult.codeMatch ? task.phone : undefined;
-      self.updateUserLoginCountAndPhone(task.userId, false, newPhone).then(() => {
-        task._state = 'sms_code_matching_completed';
+        let newPhone: string = codeMatch && task.userId ? task.phone : undefined;
+        return self.updateUserLoginCountAndPhone(task.userId, codeMatch, newPhone);
+      }).then(() => {
+        task._new_state = 'completed';
         self.resolveTask(queue, task, resolve, reject);
       }, (error: any) => {
         self.rejectTask(queue, task, error, reject);
@@ -170,20 +148,77 @@ export class AuthenticationQueueProcessor extends QueueProcessor {
     return queue;
   }
 
-  private processEmailCodeMatchingQueue(queueRef: any) {
+  private processEmailAuthCodeGenerationQueue() {
     let self = this;
-    let options = { 'specId': 'email_code_matching', 'numWorkers': 1, 'sanitize': false };
+    let options = { 'specId': 'code_generation', 'numWorkers': 1, sanitize: false };
+    let queueRef = self.db.ref('/emailAuthCodeGenerationQueue');
     let queue = new self.Queue(queueRef, options, (task: any, progress: any, resolve: any, reject: any) => {
       self.startTask(queue, task);
-      let codeMatch = task.submittedEmailAuthenticationCode == task.emailAuthenticationCode;
-      if (codeMatch) {
-        log.debug(`submittedEmailAuthenticationCode ${task.submittedEmailAuthenticationCode} matches actual emailAuthenticationCode`);
-      } else {
-        log.debug(`submittedEmailAuthenticationCode ${task.submittedEmailAuthenticationCode} does not match actual emailAuthenticationCode ${task.emailAuthenticationCode}`);
-      }
-      self.updateUserLoginCountAndPhone(task.userId, false, undefined).then(() => {
-        task.emailAuthenticationResult = { codeMatch: codeMatch };
-        task._state = 'email_code_matching_completed';
+      self.lookupUsersByEmail(task.email).then((matchingUsers) => {
+        if (_.isEmpty(matchingUsers)) {
+          log.info(`no matching user found for ${task.email}`);
+          task._new_state = "canceled_because_user_not_invited";
+          self.resolveTask(queue, task, resolve, reject);
+          return;
+        }
+
+        let activeUsers = _.reject(matchingUsers, 'disabled');
+        if (_.isEmpty(activeUsers)) {
+          let disabledUser: any = _.first(matchingUsers);
+          log.info(`found matching user ${disabledUser.userId} for ${task.email} but user was disabled`);
+          task._new_state = "canceled_because_user_disabled";
+          self.resolveTask(queue, task, resolve, reject);
+          return;
+        }
+
+        // TODO: handle case where there are multiple invitations; for now, choose first user
+        let matchingUser: any = _.first(activeUsers);
+        log.debug(`matching user with userId ${matchingUser.userId} found for ${task.email}`);
+        task.userId = matchingUser.userId;
+        self.sendAuthenticationCodeViaEmail(task.email).then((authenticationCode: string) => {
+          task.authenticationCode = authenticationCode;
+          task._new_state = "completed";
+          self.resolveTask(queue, task, resolve, reject);
+        }, (error) => {
+          self.rejectTask(queue, task, error, reject);
+        });
+      }, (error) => {
+        self.rejectTask(queue, task, error, reject);
+      });
+    });
+    return queue;
+  }
+
+  private processEmailAuthCodeMatchingQueue() {
+    let self = this;
+    let options = { 'specId': 'code_matching', 'numWorkers': 1, 'sanitize': false };
+    let queueRef = self.db.ref('/emailAuthCodeMatchingQueue');
+    let queue = new self.Queue(queueRef, options, (task: any, progress: any, resolve: any, reject: any) => {
+      self.startTask(queue, task);
+
+      let parentTaskRef = self.db.ref(`/emailAuthCodeGenerationQueue/tasks/${task._id}`);
+      parentTaskRef.once('value').then((snapshot: firebase.database.DataSnapshot) => {
+        let parentTask = snapshot.val();
+        if (!parentTask) {
+          self.rejectTask(queue, task, `could not find parent task ${parentTaskRef.toString()}`, reject);
+          return;
+        }
+
+        task.authenticationCodeSubmittedByUser = parentTask.authenticationCode;
+        task.email = parentTask.email;
+        task.userId = parentTask.userId;
+
+        return self.updateUserLoginCountAndPhone(task.userId, false, undefined);
+      }).then(() => {
+
+        let codeMatch = task.authenticationCodeSubmittedByUser == task.authenticationCode;
+        if (codeMatch) {
+          log.debug(`authenticationCodeSubmittedByUser ${task.authenticationCodeSubmittedByUser} matches actual authenticationCode`);
+        } else {
+          log.debug(`authenticationCodeSubmittedByUser ${task.authenticationCodeSubmittedByUser} does not match actual authenticationCode ${task.authenticationCode}`);
+        }
+        task.result = { codeMatch: codeMatch };
+        task._state = 'completed';
         self.resolveTask(queue, task, resolve, reject);
       }, (error: any) => {
         self.rejectTask(queue, task, error, reject);
@@ -215,17 +250,17 @@ export class AuthenticationQueueProcessor extends QueueProcessor {
     });
   }
 
-  private sendEmailAuthenticationCode(email: string): Promise<string> {
+  private sendAuthenticationCodeViaEmail(email: string): Promise<string> {
     let self = this;
     return new Promise((resolve, reject) => {
-      let emailAuthenticationCode = self.generateAuthenticationCode();
-      let messageText = `Your UR Money authentication code is ${emailAuthenticationCode}`;
+      let authenticationCode = self.generateAuthenticationCode();
+      let messageText = `Your UR Money authentication code is ${authenticationCode}`;
       self.sendEmail(
         email,
         'Your UR Money Authentication Code',
-        `Your UR Money authentication code is ${emailAuthenticationCode}`
+        `Your UR Money authentication code is ${authenticationCode}`
       ).then(() => {
-        resolve(emailAuthenticationCode);
+        resolve(authenticationCode);
       }, (error: any) => {
         reject(error);
       });
@@ -257,13 +292,13 @@ export class AuthenticationQueueProcessor extends QueueProcessor {
     });
   }
 
-  private sendSmsAuthenticationCode(phone: string): Promise<string> {
+  private sendAuthenticationCodeViaSms(phone: string): Promise<string> {
     let self = this;
     return new Promise((resolve, reject) => {
-      let smsAuthenticationCode = self.generateAuthenticationCode();
-      let messageText = `Your UR Money authentication code is ${smsAuthenticationCode}`;
+      let authenticationCode = self.generateAuthenticationCode();
+      let messageText = `Your UR Money authentication code is ${authenticationCode}`;
       self.sendSms(phone, messageText).then(() => {
-        resolve(smsAuthenticationCode);
+        resolve(authenticationCode);
       }, (error: any) => {
         reject(error);
       });
