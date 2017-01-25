2bfc8443bdd53ae2e8d722176d2532029d197138
Xavier Perez <xap5xap@gmail.com>
2017-01-25 17:03:14 -0500

    Validate phone is not registered before signUp

--- lib/sponsor_lookup.ts
+++ lib/sponsor_lookup.ts
@@ -30,6 +30,11 @@ export class SponsorLookupQueueProcessor extends QueueProcessor {
     let queue = new self.Queue(queueRef, options, (task: any, progress: any, resolve: any, reject: any) => {
       self.startTask(queue, task);
 
+      if (!task.sponsorReferralCode && !task.phone) {
+        self.rejectTask(queue, task, 'expecting either a phone or a sponsorReferralCode', reject);
+        return;
+      }
+
       let resolveAsNotFound = (referralCode: string) => {
         log.info(`  no matching user found for referral code ${referralCode}`);
         task.result = { found: false };
@@ -37,26 +42,54 @@ export class SponsorLookupQueueProcessor extends QueueProcessor {
         self.resolveTask(queue, task, resolve, reject);
       };
 
-      self.lookupUserByReferralCode(task.sponsorReferralCode).then((sponsor: any) => {
-        if (!sponsor) {
-          log.info(`  no sponsor found with referral code ${task.sponsorReferralCode}`);
-          resolveAsNotFound(task.sponsorReferralCode);
-          return;
-        }
-
-        task.result = { found: true, sponsorName: sponsor.name ? sponsor.name : `${sponsor.firstName} ${sponsor.lastName}`, disabled: sponsor.disabled || false }
-
-        task._new_state = "sponsor_lookup_finished";
+      let resolveAsPhoneAlreadyRegistered = (phone: string) => {
+        log.info(`Phone already registered ${phone}`);
+        task.result = { found: true };
+        task._new_state = "sponsor_lookup_canceled_because_phone_already_registered";
         self.resolveTask(queue, task, resolve, reject);
+      };
+      if (task.phone) {
+        self.lookupIfPhoneAlreadyExists(task.phone).then(user => {
+          if (user) {
+            resolveAsPhoneAlreadyRegistered(task.phone);
+            return;
+          }
+          task.result = { found: false };
+          task._new_state = "sponsor_lookup_finished";
+          self.resolveTask(queue, task, resolve, reject);
+        }, (error) => {
+          self.rejectTask(queue, task, error, reject);
+        });
+      } else {
+        self.lookupUserByReferralCode(task.sponsorReferralCode).then((sponsor: any) => {
+          if (!sponsor) {
+            log.info(`  no sponsor found with referral code ${task.sponsorReferralCode}`);
+            resolveAsNotFound(task.sponsorReferralCode);
+            return;
+          }
 
-      }, (error) => {
-        self.rejectTask(queue, task, error, reject);
-      });
+          task.result = { found: true, sponsorName: sponsor.name ? sponsor.name : `${sponsor.firstName} ${sponsor.lastName}`, disabled: sponsor.disabled || false }
+
+          task._new_state = "sponsor_lookup_finished";
+          self.resolveTask(queue, task, resolve, reject);
+
+        }, (error) => {
+          self.rejectTask(queue, task, error, reject);
+        });
+      }
     });
 
     return queue;
   }
 
+  lookupIfPhoneAlreadyExists(phone: string): Promise<any> {
+    let self = this;
+    return new Promise((resolve, reject) => {
+      self.lookupUsersByPhone(phone).then((matchingUsers: any) => {
+        resolve(matchingUsers[0]);
+      });
+    });
+  }
 
   lookupUserByReferralCode(referralCode: string): Promise<any> {
     let self = this;
