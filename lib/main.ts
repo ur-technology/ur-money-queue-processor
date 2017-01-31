d3d18deae49f300f1048b5a9a94b03dc29f2b6fd
John Reitano <jreitano@gmail.com>
2017-01-31 14:04:05 -0800

    change authentication to use password

--- lib/main.ts
+++ lib/main.ts
@@ -6,22 +6,16 @@ import * as _ from 'lodash';
 import {QueueProcessor} from './queue_processor';
 import {ChatQueueProcessor} from './chat';
 import {IdentityAnnouncementQueueProcessor} from './identity_announcement';
-import {InvitationQueueProcessor} from './invitation';
-import {AuthenticationQueueProcessor} from './authentication';
-import {PhoneAuthQueueProcessor} from './phone_auth';
 import {PhoneLookupQueueProcessor} from './phone_lookup';
+import {SignUpQueueProcessor} from './sign_up';
+import {SignInQueueProcessor} from './sign_in';
 import {UrTransactionImportQueueProcessor} from './ur_transaction_import';
-import {PrefineryImportQueueProcessor} from './prefinery_import';
-import {PrefineryCleanupQueueProcessor} from './prefinery_cleanup';
-import {SponsorLookupQueueProcessor} from './sponsor_lookup';
 
 if (!process.env.NODE_ENV) {
   dotenv.config(); // if running on local machine, load config vars from .env file, otherwise these come from heroku
 }
 
 log.setDefaultLevel(process.env.LOG_LEVEL || "info")
-
-log.info(`starting with NODE_ENV ${process.env.NODE_ENV} and FIREBASE_PROJECT_ID ${process.env.FIREBASE_PROJECT_ID}`);
 
 let serviceAccount = require(`../serviceAccountCredentials.${process.env.FIREBASE_PROJECT_ID}.json`);
 let admin = require("firebase-admin");
@@ -38,19 +32,24 @@ QueueProcessor.Queue = require('firebase-queue');
 let queueProcessors = _.map([
   ChatQueueProcessor,
   IdentityAnnouncementQueueProcessor,
-  InvitationQueueProcessor,
-  AuthenticationQueueProcessor,
-  PhoneAuthQueueProcessor,
   PhoneLookupQueueProcessor,
-  UrTransactionImportQueueProcessor,
-  PrefineryImportQueueProcessor,
-  PrefineryCleanupQueueProcessor,
-  SponsorLookupQueueProcessor
+  SignUpQueueProcessor,
+  SignInQueueProcessor,
+  UrTransactionImportQueueProcessor
 ], (queueProcessorClass) => {
   return new queueProcessorClass();
 });
 
-let initializerPromises = _.flatten(_.map(queueProcessors, (p) => { return p.enabled() ? p.init() : []; }));
+log.info(`starting with NODE_ENV ${process.env.NODE_ENV} and FIREBASE_PROJECT_ID ${process.env.FIREBASE_PROJECT_ID}`);
+_.each(queueProcessors, (p) => {
+  if (p.enabled()) {
+    log.info(`processing enabled for ${p.className()}`);
+  }
+});
+
+let initializerPromises = _.flatten(_.map(queueProcessors, (p) => {
+  return p.enabled() ? p.init() : [];
+}));
 
 let queues: any[] = [];
 Promise.all(initializerPromises).then(values => {
