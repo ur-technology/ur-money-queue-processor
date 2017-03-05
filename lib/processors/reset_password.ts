5ca6c1eb4231c0f68b40317f651a14f510b85a6b
Haohong Xu <weidai1122@gmail.com>
2017-03-05 15:05:43 +0800

    Add send_reset_code spec processor

--- lib/processors/reset_password.ts
+++ lib/processors/reset_password.ts
@@ -2,9 +2,18 @@ import * as firebase from 'firebase';
 import * as _ from 'lodash';
 import * as log from 'loglevel';
 import { QueueProcessor } from './queue_processor';
+import { SendGridService } from '../services/sendgrid.service';
 
 
 export class ResetPasswordQueueProcessor extends QueueProcessor {
+    private sendGridService: SendGridService;
+
+    constructor() {
+        super();
+
+        this.sendGridService = new SendGridService(process.env.SENDGRID_API_KEY);
+    }
+
     init(): Promise<any>[] {
 
         this._queueName = 'resetPasswordQueue';
@@ -40,13 +49,87 @@ export class ResetPasswordQueueProcessor extends QueueProcessor {
      * The data provided are:
      *  @email: email of user who requested to reset password
      */
-    private processSendResetCodeSpec() {}
+    private processSendResetCodeSpec() {
+        let self = this;
+        let options = {
+            specId: 'send_reset_code',
+            numWorkers: 8,
+            sanitize: false
+        };
+        let queue = new self.Queue(
+            self._queueRef,
+            options,
+            (task: any, progress: any, resolve: any, reject: any) => {
+                let resetCode: string;
+
+                self.startTask(queue, task);
+
+                // Check email emptiness
+                if (!task.email) {
+                    throw 'send_reset_code_canceled_because_email_empty';
+                }
+                
+                // Check if email exists
+                self.lookupUsersByEmail(task.email)
+                    .then((matchingUsers: any[]) => {
+                        if (_.isEmpty(matchingUsers)) {
+                            throw 'send_reset_code_canceled_because_user_not_found';
+                        }
+
+                        let user = matchingUsers[0];
+                        
+                        task.userId = user.userId;
+                        if (user.disabled) {
+                            throw 'send_reset_code_canceled_because_user_disabled';
+                        }
+
+                        // Generate reset code
+                        resetCode = self.generateResetCode();
+                        // Send reset code email
+                        return self.sendResetCodeEmail(task.email, resetCode);
+                    })
+                    .then((response: any) => {
+                        // Update task
+                        task.resetCode = resetCode;
+                        // Resolve task
+                        self.resolveTask(queue, task, resolve, reject);
+                    }, (error: any) => {
+                        self.rejectTask(queue, task, error, reject);
+                    });
+            }
+        );
+        return queue;
+    }
+
+    private generateResetCode() {
+        let chars: string = '23456789abcdefghjkmnpqrstuvwxyzABCDEFGHJKMNPQRSTUVWXYZ';
+        return _.sampleSize(chars, 6).join('');
+    }
+
+    private sendResetCodeEmail(email, resetCode) {
+        let from = process.env.UR_SUPPORT_EMAIL;
+        let to = email;
+        let subject = 'Password reset code';
+        let content = `Reset code is ${resetCode}`;
+        let contentType = 'text/plain';
+        
+        this.sendGridService
+            .send(
+                from,
+                to,
+                subject,
+                content,
+                contentType
+            );
+    }
 
     /**
      * Process reset_password spec
      * 
      * The data provided are:
-     *  @reset_code: reset code
+     *  @server_reset_code: server reset code
+     *  @reset_code:        reset code entered by user
+     *  @new_password:      new password entered by user
      */
     private processResetPasswordSpec() {}
 }
