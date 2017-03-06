6726b332b0f1099075f01d7b19e95bdb9e059f29
Haohong Xu <weidai1122@gmail.com>
2017-03-07 00:33:49 +0800

    Update reset_password queue processors to save result

--- lib/processors/reset_password.ts
+++ lib/processors/reset_password.ts
@@ -10,30 +10,40 @@ export class ResetPasswordQueueProcessor extends QueueProcessor {
     private passwordService: PasswordService;
     private sendGridService: SendGridService;
 
+    private specs: any;
+
     constructor() {
         super();
 
         this._queueName = 'resetPasswordQueue';
         this._queueRef = this.db.ref(`/${this._queueName}`);
         
+        this.specs = {
+            send_reset_code: {
+                start_state: 'send_reset_code_requested',
+                in_progress_state: 'send_reset_code_in_progress',
+                finished_state: 'send_reset_code_finished',
+                error_state: 'send_reset_code_error',
+                timeout: 5 * 60 * 1000
+            },
+            reset_password: {
+                start_state: 'reset_password_requested',
+                in_progress_state: 'reset_password_in_progress',
+                finished_state: 'reset_password_finished',
+                error_state: 'reset_password_error',
+                timeout: 5 * 60 * 1000
+            }
+        };
+        
         this.passwordService = PasswordService.getInstance();
         this.sendGridService = SendGridService.getInstance();
+
     }
 
     init(): Promise<any>[] {
         return [
-            this.ensureQueueSpecLoaded(`/${this._queueName}/specs/send_reset_code`, {
-                start_state: 'send_reset_code_requested',
-                in_progress_state: 'send_reset_code_in_progress',
-                error_state: 'send_reset_code_error',
-                timeout: 5 * 60 * 1000
-            }),
-            this.ensureQueueSpecLoaded(`/${this._queueName}/specs/reset_password`, {
-                start_state: 'reset_password_requested',
-                in_progress_state: 'reset_password_in_progress',
-                error_state: 'reset_password_error',
-                timeout: 5 * 60 * 1000
-            }),
+            this.ensureQueueSpecLoaded(`/${this._queueName}/specs/send_reset_code`, this.specs['send_reset_code']),
+            this.ensureQueueSpecLoaded(`/${this._queueName}/specs/reset_password`, this.specs['reset_password']),
             // this.addSampleTask({
             //     _state: 'send_reset_code_requested',
             //     email: 'weidai1122@gmail.com'
@@ -118,9 +128,24 @@ export class ResetPasswordQueueProcessor extends QueueProcessor {
                     })
                     .then((response: any) => {
                         // Resolve task
+                        task.result = {
+                            state: this.specs['send_reset_code']['finished_state'],
+                        };
                         self.resolveTask(queue, task, resolve, reject);
                     }, (error: any) => {
-                        self.rejectTask(queue, task, error, reject);
+                        if (_.isString(error) && /^send_reset_code_canceled_/.test(error)) {
+                            task.result = {
+                                state: error,
+                                error,
+                            };
+                            self.resolveTask(queue, task, resolve, reject);
+                        } else {
+                            task.result = {
+                                state: this.specs['send_reset_code']['error_state'],
+                                error,
+                            };
+                            self.resolveTask(queue, task, resolve, reject);
+                        }
                     });
             }
         );
@@ -192,7 +217,7 @@ export class ResetPasswordQueueProcessor extends QueueProcessor {
 
                         user = matchingUsers[0];
                         if (user.disabled) {
-                            throw 'send_reset_code_canceled_because_user_disabled';
+                            throw 'reset_password_canceled_because_user_disabled';
                         }
 
                         // Generate hashed password
@@ -207,9 +232,24 @@ export class ResetPasswordQueueProcessor extends QueueProcessor {
                     })
                     .then((response: any) => {
                         // Resolve task
+                        task.result = {
+                            state: this.specs['reset_password']['finished_state'],
+                        };
                         self.resolveTask(queue, task, resolve, reject);
                     }, (error: any) => {
-                        self.rejectTask(queue, task, error, reject);
+                        if (_.isString(error) && /^reset_password_canceled_/.test(error)) {
+                            task.result = {
+                                state: error,
+                                error,
+                            };
+                            self.resolveTask(queue, task, resolve, reject);
+                        } else {
+                            task.result = {
+                                state: this.specs['reset_password']['error_state'],
+                                error,
+                            };
+                            self.resolveTask(queue, task, resolve, reject);
+                        }
                     });
             }
         );
