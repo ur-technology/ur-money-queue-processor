0a44e215e4e8dd88e9534018554df5a0c00dd23a
Haohong Xu <weidai1122@gmail.com>
2017-03-06 17:59:58 +0800

    Add generateCode() method to password service

--- lib/services/password.service.ts
+++ lib/services/password.service.ts
@@ -1,5 +1,6 @@
-let scryptAsync = require('scrypt-async');
+import * as _ from 'lodash';
 
+let scryptAsync = require('scrypt-async');
 
 export class PasswordService {
     private static _instance: PasswordService;
@@ -11,17 +12,27 @@ export class PasswordService {
         return PasswordService._instance;
     }
 
+    generateCode(len = 6) {
+        let chars: string = '23456789abcdefghjkmnpqrstuvwxyzABCDEFGHJKMNPQRSTUVWXYZ';
+        return _.sampleSize(chars, len).join('');
+    }
+    
     hashPassword(rawPassword: string, salt: string) {
-        return scryptAsync(
-            rawPassword,
-            salt,
-            {
-                N: 16384,
-                r: 16,
-                p: 1,
-                dkLen: 64,
-                encoding: 'hex'
-            }
-        );
+        return new Promise((resolve, reject) => {
+            scryptAsync(
+                rawPassword,
+                salt,
+                {
+                    N: 16384,
+                    r: 16,
+                    p: 1,
+                    dkLen: 64,
+                    encoding: 'hex'
+                },
+                (hashedPassword: string) => {
+                    resolve(hashedPassword);
+                }
+            );
+        });
     }
 }
